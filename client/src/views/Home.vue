98e7c03ede3b3192c1646afc64bb64762a742c87
Tech Reagan <reaganekhameye@gmail.com>
2020-01-31 07:16:03 +0100

    Modify homepage text

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="home mt-3">
     <p>
-      This is a RESTful API for Intelligent Innovations Conference to manage
-      their conferences.
+      RESTful API for Intelligent Innovations Conference to manage their
+      conferences.
     </p>
+    <a
+      href="https://iic-restful.herokuapp.com/docs.html"
+      class="btn purple-background"
+      target="_blank"
+      >Api Documentation</a
+    >
   </div>
 </template>
+
+<style scoped>
+.home {
+  padding-left: 1em;
+  padding-right: 1em;
+}
+</style>
 
 <script>
 export default {
